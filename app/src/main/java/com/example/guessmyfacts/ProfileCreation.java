281a1578c19cdb388367967bb37cb1b115141f57
diegoealvarez14 <alvarez.170@buckeyemail.osu.edu>
2019-03-08 23:09:51 -0500

    added update button to UI

--- app/src/main/java/com/example/guessmyfacts/ProfileCreation.java
+++ app/src/main/java/com/example/guessmyfacts/ProfileCreation.java
@@ -54,6 +54,17 @@ public class ProfileCreation extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        Button updateButton = findViewById(R.id.updateButton);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+
+            }
+        });
+
+
+
         Button submitButton = findViewById(R.id.submitButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
